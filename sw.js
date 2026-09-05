da58591ec9049829fae5cc226484236cba0c89b2
jen <sjenwork@gmail.com>
2026-09-05 12:44:28 +0800

    fix: prefer fresh assets over pwa cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'multiplication-master-v20260905-124212';
+const CACHE_NAME = 'multiplication-master-v20260905-124411';
 const APP_SHELL = [
     '/',
     '/index.html',
@@ -24,9 +24,23 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     if (event.request.method !== 'GET') return;
+    const requestUrl = new URL(event.request.url);
+    const latestFirst = event.request.mode === 'navigate'
+        || requestUrl.pathname.endsWith('.html')
+        || requestUrl.pathname.endsWith('/app.js');
+    if (latestFirst) {
+        event.respondWith(
+            fetch(event.request).then((response) => {
+                const copy = response.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+                return response;
+            }).catch(() => caches.match(event.request, { ignoreSearch: true }).then((cached) => cached || caches.match('/index.html')))
+        );
+        return;
+    }
     event.respondWith(
         caches.match(event.request, { ignoreSearch: true }).then((cached) => cached || fetch(event.request).then((response) => {
-            if (new URL(event.request.url).origin === self.location.origin) {
+            if (requestUrl.origin === self.location.origin) {
                 const copy = response.clone();
                 caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
             }
